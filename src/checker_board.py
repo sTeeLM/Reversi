fa8c27fae127100d7a25e7647d61a10a106b0fa0
sTeeL <steel.mentalgmail.com>
2015-05-12 01:32:46 +0000

    add main.py

--- src/checker_board.py
+++ src/checker_board.py
@@ -15,7 +15,7 @@ class CheckerBoard(object):
     STATUS_DRAW = 3
     BLACK_CHECK_PATTEN = re.compile(r'^bw+b.*')
     WHITE_CHECK_PATTEN = re.compile(r'^wb+w.*')
-    
+
     class Diff(object):
         def __init__(self, x, y, f, t):
             self.x = x
@@ -24,11 +24,11 @@ class CheckerBoard(object):
             self.t = t
         def __str__(self):
             return '[x={0}, y={1}, {2}->{3}]'. \
-                format(self.x, self.y, 
-                       'b' if self.f == Player.PLAYER_BLACK else \
-                       ('0' if self.f == 0 else 'w'), 
-                       'b' if self.t == Player.PLAYER_BLACK else \
-                       ('0' if self.t == 0 else 'w'))
+            format(self.x, self.y,
+            'b' if self.f == Player.PLAYER_BLACK else \
+            ('0' if self.f == 0 else 'w'),
+            'b' if self.t == Player.PLAYER_BLACK else \
+            ('0' if self.t == 0 else 'w'))
     class Diffs(object):
         def __init__(self, left, right, up, down):
             self.left = left
@@ -41,7 +41,7 @@ class CheckerBoard(object):
                 raise ReversiInvalidParam('pushed diff must be CheckerBoard.Diff')
             self.diffs.append(diff)
             return self
-            
+
         def _val_direction_str(self, left, right, up, down):
             if (left, right, up, down) == (1, 0, 0, 0):
                 return 'L'
@@ -58,22 +58,22 @@ class CheckerBoard(object):
             elif (left, right, up, down) == (0, 1, 0, 1):
                 return 'RD'
             elif (left, right, up, down) == (1, 0, 0, 1):
-                return 'LD' 
+                return 'LD'
             elif (left, right, up, down) == (0, 0, 0, 0):
                 return 'C'
             else:
                 raise ReversiInvalidParam('invalid direction')
         def __str__(self):
             tmp = '[{0}'.format(self.\
-                                _val_direction_str(self.left, 
-                                                   self.right,
-                                                   self.up, 
-                                                   self.down))
+            _val_direction_str(self.left,
+            self.right,
+            self.up,
+            self.down))
             for i in self.diffs:
                 tmp+=str(i)
             tmp += ']'
             return tmp
-    
+
     class Move(object):
         INVALID_POS = -1
         def __init__(self, role, x, y, skip=False):
@@ -85,24 +85,24 @@ class CheckerBoard(object):
                 raise ReversiInvalidParam('invalid role')
         def __str__(self):
             return '[{0},{1},{2},{3}]'.format(
-                    'b' if self.role == Player.PLAYER_BLACK else 'w',
-                    'T' if self.skip else 'F',
-                    self.x, self.y)
-            
+            'b' if self.role == Player.PLAYER_BLACK else 'w',
+            'T' if self.skip else 'F',
+            self.x, self.y)
+
     def __init__(self, size, w_player, b_player):
         self.round = 1
         self.size = size
         self.w_player = w_player
         self.b_player = b_player
         if  not isinstance(self.w_player, Player) or \
-            not isinstance(self.b_player, Player) or  \
-            (w_player.role != Player.PLAYER_WHITE) or \
-            (b_player.role != Player.PLAYER_BLACK) :
+        not isinstance(self.b_player, Player) or  \
+        (w_player.role != Player.PLAYER_WHITE) or \
+        (b_player.role != Player.PLAYER_BLACK) :
             raise ReversiInvalidParam('invalid player')
         if size <= 0:
             raise ReversiInvalidParam('invalid size')
         self.data_array = [[0 for i in xrange(0, size)] \
-                           for j in xrange(0, size)]
+        for j in xrange(0, size)]
         self.data_array[3][3] = Player.PLAYER_WHITE
         self.data_array[4][4] = Player.PLAYER_WHITE
         self.data_array[3][4] = Player.PLAYER_BLACK
@@ -114,12 +114,12 @@ class CheckerBoard(object):
     def dump(self, fil):
         # status
         fil.write('+++status: {0}\n'.\
-                  format(self._status_to_string(self.status)))
+        format(self._status_to_string(self.status)))
         # next player
         fil.write('+++next player: {0}\n'.format(
-                self.next_player.name if self.next_player else 'None'))
+        self.next_player.name if self.next_player else 'None'))
         #round
-        fil.write('+++round:{0}\n'.format(self.round))   
+        fil.write('+++round:{0}\n'.format(self.round))
         # data array
         fil.write('+++checker board:\n')
         fil.write('+++ ')
@@ -137,14 +137,14 @@ class CheckerBoard(object):
                     mystr += '0'
             mystr += '\n'
             fil.write(mystr)
-        
+
     def run(self):
         while CheckerBoard.STATUS_IN_PROGRESS == self.status:
             diffs_list=[]
             moves = self.get_possible_moves(self.next_player)
             move = self.next_player.make_decision(self, moves)
             self.play(move, diffs_list).notify_diff(diffs_list)
-        
+
     def play(self, move, diffs_list):
         # 一般合法性检查
         if not isinstance(move, CheckerBoard.Move):
@@ -155,43 +155,43 @@ class CheckerBoard(object):
             raise ReversiInvalidMove('game already over')
         # 如果不是空步骤，并且不是可能的步骤
         if not move.skip and not self.\
-        		_is_possible_move(move.x, move.y, move.role):
+        _is_possible_move(move.x, move.y, move.role):
             raise ReversiInvalidMove('not a possible move')
-        
+
         saved_player = self.next_player
-        
+
         # 如果棋盘可以下一个子，但是尝试略过，是非法的
         tmp = self.get_possible_moves(self.next_player)
         if len(tmp) != 1 or not tmp[0].skip:
             if move.skip:
                 raise ReversiInvalidMove('try to skip, but one can play')
-         
-        # 开始搞了    
-        self.round += 1  
+
+        # 开始搞了
+        self.round += 1
         r_player = self.b_player \
-            if self.next_player is self.w_player else self.w_player
-                
+        if self.next_player is self.w_player else self.w_player
+
         # 如果不是空步骤
         if not move.skip:
             diffs_list.append(
-                              CheckerBoard.Diffs(0,0,0,0).\
-                              push(CheckerBoard.Diff(move.x, move.y,
-                              self.data_array[move.x][move.y],
-                              move.role)))
+            CheckerBoard.Diffs(0,0,0,0).\
+            push(CheckerBoard.Diff(move.x, move.y,
+            self.data_array[move.x][move.y],
+            move.role)))
             self.data_array[move.x][move.y] = move.role
             # 下了个子，加一分
             self.next_player.score += 1
             # 翻转对手的棋子，更新两者的得分
             self._try_revert(move.x, move.y, self.next_player, \
-                            r_player, diffs_list)
+            r_player, diffs_list)
         else:
             pass
-        
+
         # 转换角色
         self.next_player = r_player
         r_player = self.b_player \
-            if self.next_player is self.w_player else self.w_player      
-            
+        if self.next_player is self.w_player else self.w_player
+
         # 如果本次是略过，并且另一方也没法走子了，结束STATUS_IN_PROGRESS
         tmp = self.get_possible_moves(self.next_player)
         if len(tmp) == 1 and tmp[0].skip and move.skip:
@@ -203,10 +203,10 @@ class CheckerBoard(object):
                 self._end_progress(self.next_player, r_player)
         # 如果棋盘满了，结束STATUS_IN_PROGRESS
         elif self.b_player.score + self.w_player.score == self.size ** 2:
-                self._end_progress(self.next_player, r_player)
-        
+            self._end_progress(self.next_player, r_player)
+
         return saved_player
-        
+
     def _status_to_string(self, status):
         if status == CheckerBoard.STATUS_BLACK_WIN:
             return "Black Win"
@@ -218,7 +218,7 @@ class CheckerBoard(object):
             return "Draw"
         else:
             return "Unknown"
-            
+
     def _end_progress(self, player, r_player):
         if self.next_player.score == r_player.score:
             self.status = CheckerBoard.STATUS_DRAW
@@ -233,7 +233,7 @@ class CheckerBoard(object):
             else:
                 self.status = CheckerBoard.STATUS_BLACK_WIN
         self.next_player = None
-    
+
     def get_possible_moves(self, player):
         temp = []
         for x in xrange(0, self.size):
@@ -241,11 +241,11 @@ class CheckerBoard(object):
                 if self._is_possible_move(x, y, player.role):
                     temp.append(CheckerBoard.Move(player.role, x, y))
         if self.status == CheckerBoard.STATUS_IN_PROGRESS and not temp:
-            temp.append(CheckerBoard.Move(player.role, 
-                        CheckerBoard.Move.INVALID_POS,
-                        CheckerBoard.Move.INVALID_POS, True))
+            temp.append(CheckerBoard.Move(player.role,
+            CheckerBoard.Move.INVALID_POS,
+            CheckerBoard.Move.INVALID_POS, True))
         return temp
-    
+
     def dup(self):
         wp = self.w_player.dup()
         bp = self.b_player.dup()
@@ -260,15 +260,15 @@ class CheckerBoard(object):
         # already have black or white
         if self.data_array[x][y] != 0:
             return False
-        # test all directions: 
+        # test all directions:
         return self._test_direction(x, y, role, 1, 0, 0, 0) or \
-            self._test_direction(x, y, role, 1, 0, 1, 0) or \
-            self._test_direction(x, y, role, 0, 0, 1, 0) or \
-            self._test_direction(x, y, role, 0, 1, 1, 0) or \
-            self._test_direction(x, y, role, 0, 1, 0, 0) or \
-            self._test_direction(x, y, role, 0, 1, 0, 1) or \
-            self._test_direction(x, y, role, 0, 0, 0, 1) or \
-            self._test_direction(x, y, role, 1, 0, 0, 1)
+        self._test_direction(x, y, role, 1, 0, 1, 0) or \
+        self._test_direction(x, y, role, 0, 0, 1, 0) or \
+        self._test_direction(x, y, role, 0, 1, 1, 0) or \
+        self._test_direction(x, y, role, 0, 1, 0, 0) or \
+        self._test_direction(x, y, role, 0, 1, 0, 1) or \
+        self._test_direction(x, y, role, 0, 0, 0, 1) or \
+        self._test_direction(x, y, role, 1, 0, 0, 1)
 
     def _test_direction(self, x, y, role, left, right, up, down):
         temp = []
@@ -289,10 +289,10 @@ class CheckerBoard(object):
             temp.append(self.data_array[x][y])
         ret = self._s_check(temp, role)
         logger.dbg('_test_direction {0} {1} {2} {3} {4} {5} {6} {7}'.
-                   format(_x, _y, role, left, right, up, down, 
-                          'OK' if ret else 'Fail'))
+        format(_x, _y, role, left, right, up, down,
+        'OK' if ret else 'Fail'))
         return ret
-    
+
     def _s_check(self, s, role):
         '''
         return true if s is [b, w, w...b, 0...] if role is b
@@ -301,7 +301,7 @@ class CheckerBoard(object):
         if not s or len(s) < 3:
             return False
         if role == Player.PLAYER_WHITE and s[0] != Player.PLAYER_WHITE or \
-            role == Player.PLAYER_BLACK and s[0] != Player.PLAYER_BLACK:
+        role == Player.PLAYER_BLACK and s[0] != Player.PLAYER_BLACK:
             return False
         temp = ''
         for p in s:
@@ -312,16 +312,16 @@ class CheckerBoard(object):
             else:
                 temp += '0'
         if role == Player.PLAYER_WHITE and \
-        		CheckerBoard.WHITE_CHECK_PATTEN.match(temp) \
-            or role == Player.PLAYER_BLACK and \
-            CheckerBoard.BLACK_CHECK_PATTEN.match(temp):
+        CheckerBoard.WHITE_CHECK_PATTEN.match(temp) \
+        or role == Player.PLAYER_BLACK and \
+        CheckerBoard.BLACK_CHECK_PATTEN.match(temp):
             logger.dbg('_s_check {0} {1} OK'.format(temp, role))
             return True
         logger.dbg('_s_check {0} {1} Fail'.format(temp, role))
         return False
 
-    def _try_revert_direction(self, x, y, player, 
-                              r_player, left, right, up, down, diffs_list):
+    def _try_revert_direction(self, x, y, player,
+    r_player, left, right, up, down, diffs_list):
         if self._test_direction(x, y, player.role, left, right, up, down):
             tmp = CheckerBoard.Diffs(left, right, up, down)
             _x = x
@@ -340,41 +340,40 @@ class CheckerBoard(object):
                 if not self._s_revert(x, y, player, r_player, tmp):
                     break
             diffs_list.append(tmp)
-            
+
     def _try_revert(self, x, y, player, r_player, diffs_list):
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 1, 0, 0, 0, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 1, 0, 1, 0, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 0, 0, 1, 0, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 0, 1, 1, 0, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 0, 1, 0, 0, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 0, 1, 0, 1, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 0, 0, 0, 1, diffs_list)
-        self._try_revert_direction(x, y, player, 
-                                   r_player, 1, 0, 0, 1, diffs_list)
- 
+        self._try_revert_direction(x, y, player,
+        r_player, 1, 0, 0, 0, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 1, 0, 1, 0, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 0, 0, 1, 0, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 0, 1, 1, 0, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 0, 1, 0, 0, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 0, 1, 0, 1, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 0, 0, 0, 1, diffs_list)
+        self._try_revert_direction(x, y, player,
+        r_player, 1, 0, 0, 1, diffs_list)
+
     def _s_revert(self, x, y, player, r_player, diffs):
         if self.data_array[x][y] == r_player.role:
             logger.dbg('_s_revert: {0} {1} {2}->{3}'.\
-                       format(x, y, self.data_array[x][y], player.role))
+            format(x, y, self.data_array[x][y], player.role))
             self.data_array[x][y] = player.role
             player.score += 1
             r_player.score -= 1
-            diffs.push(CheckerBoard.Diff(x, y, 
-                                         r_player.role, player.role))
+            diffs.push(CheckerBoard.Diff(x, y,
+            r_player.role, player.role))
         elif self.data_array[x][y] == player.role:
             logger.dbg('_s_revert: {0} {1} {2}->{3} break'.\
-                       format(x, y, self.data_array[x][y], player.role))
+            format(x, y, self.data_array[x][y], player.role))
             return False
         else:
             logger.dbg('_s_revert: {0} {1} {2} break'.\
-                       format(x, y, self.data_array[x][y]))
+            format(x, y, self.data_array[x][y]))
             return False
         return True
-                
--- src/main.py
+++ src/main.py
@@ -1,17 +1,86 @@
-# coding: utf-8
+# Reversi Game
 
-import ui
 from scene import *
+from functools import partial
+import sound
 
-class MainView (ui.View):
-	def __init__(self):
-		pass
+class Game (Scene):
+    def setup(self):
+        self.root_layer = Layer(self.bounds)
+        for effect in ['Click_1', 'Click_2', 'Coin_2', 'Coin_5']:
+            sound.load_effect(effect)
 
-v = ui.load_view('main')
-if ui.get_screen_size()[1] >= 768:
-	# iPad
-	v.present('popover')
-else:
-	# iPhone
-	v.present(orientations=['portrait'])
+        self.deal()
 
+    def draw(self):
+        background(0.0, 0.2, 0.3)
+        self.root_layer.update(self.dt)
+        self.root_layer.draw()
+
+    def deal(self):
+        images = ['Rabbit_Face', 'Mouse_Face', 'Cat_Face']
+        for image in images:
+            load_image(image)
+#        self.root_layer.sublayers = []
+        self.cards = []
+        card_size = 48 if self.size.w > 700 else 32
+        width = (card_size + 2) * 8
+        offset = Point((self.size.w - width)/2,
+                       (self.size.h - width)/2)
+        for i in xrange(0, 64):
+            x, y = i % 8, i / 8
+            card = Layer(Rect(offset.x + x * (card_size + 2),
+                              offset.y + y * (card_size + 2),
+                              card_size, card_size))
+            card.card_image = images[0]
+            card.background = Color(0.9, 0.9, 0.9)
+            card.stroke = Color(1, 1, 1)
+            card.stroke_weight = 4.0
+            card.index_x = x
+            card.index_y = y
+            self.add_layer(card)
+            self.cards.append(card)
+        self.touch_disabled = False
+
+    def touch_began(self, touch):
+        if self.touch_disabled:
+            return
+        for card in self.cards:
+            if touch.location in card.frame:
+                def reveal_card():
+                    card.image = card.card_image
+                    card.animate('scale_x', 1.0, 0.15,
+                                 completion=None)
+#                self.touch_disabled = True
+                card.animate('scale_x', 0.0, 0.15,
+                             completion=reveal_card)
+                card.scale_y = 1.0
+                card.animate('scale_y', 0.9, 0.15, autoreverse=True)
+                sound.play_effect('Click_1')
+                break
+
+    def new_game(self):
+        sound.play_effect('Coin_2')
+        self.deal()
+        self.root_layer.animate('scale_x', 1.0)
+        self.root_layer.animate('scale_y', 1.0)
+
+    def win(self):
+        self.delay(0.5, partial(sound.play_effect, 'Powerup_2'))
+        font_size = 100 if self.size.w > 700 else 50
+        text_layer = TextLayer('Well Done!', 'Futura', font_size)
+        text_layer.frame.center(self.bounds.center())
+        overlay = Layer(self.bounds)
+        overlay.background = Color(0, 0, 0, 0)
+        overlay.add_layer(text_layer)
+        self.add_layer(overlay)
+        overlay.animate('background', Color(0.0, 0.2, 0.3, 0.7))
+        text_layer.animate('scale_x', 1.3, 0.3, autoreverse=True)
+        text_layer.animate('scale_y', 1.3, 0.3, autoreverse=True)
+#        self.touch_disabled = True
+        self.root_layer.animate('scale_x', 0.0, delay=2.0,
+                                curve=curve_ease_back_in)
+        self.root_layer.animate('scale_y', 0.0, delay=2.0,
+                                curve=curve_ease_back_in,
+                                completion=self.new_game)
+run(Game())
